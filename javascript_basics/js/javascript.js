98cafe5b7357de965d91b3c8c7a05a8bbccf5ae6
dahwild22 <danwilderdesign@gmail.com>
2021-06-27 02:25:15 -0600

    javascript_basics Fundamentals Part 1

---
+++ javascript_basics/js/javascript.js
@@ -0,0 +1,24 @@
+let admin, name, planet, current_visitor, x, y, z, a, b, c; // declare 2 variables
+
+name = 'John'; // assign John to name
+
+admin = name;
+
+planet = 'Earth';
+
+x = 3.14;
+
+y = 3;
+
+z = x * y;
+
+a = 0xFF;
+
+b = 128;
+
+c = new Number(123);
+
+d = 10/2;
+
+alert(admin + ' ' + z + ' ' + b.toString(2) + ' ' + d); // shows the value stored in the variable message
+
