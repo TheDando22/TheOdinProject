88ae794a75932c5d3e4c8a3bfa317bd00ab8abe2
dahwild22 <danwilderdesign@gmail.com>
2021-06-29 01:27:59 -0600

    simple draw function

--- javascript_basics/js/javascript.js
+++ javascript_basics/js/javascript.js
@@ -1,24 +1,2 @@
-let admin, name, planet, current_visitor, x, y, z, a, b, c; // declare 2 variables
-
-name = 'John'; // assign John to name
-
-admin = name;
-
-planet = 'Earth';
-
-x = 3.14;
-
-y = 3;
-
-z = x * y;
-
-a = 0xFF;
-
-b = 128;
-
-c = new Number(123);
-
-d = 10/2;
-
-alert(admin + ' ' + z + ' ' + b.toString(2) + ' ' + d); // shows the value stored in the variable message
-
+let a = 123.412;
+let b = random(Number);
