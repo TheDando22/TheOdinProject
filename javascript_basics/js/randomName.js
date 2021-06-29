4a210a0ae74fad2f512a6918bd1074ed5c4c19ad
dahwild22 <danwilderdesign@gmail.com>
2021-06-29 12:53:31 -0600

    javascript basics mdn tasks and odin project exercises

--- javascript_basics/js/randomName.js
+++ javascript_basics/js/randomName.js
@@ -1,19 +1,18 @@
 let names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert', 'Jada']
-let para = document.createElement('p');
+let para = document.querySelector('p');
+let section = document.getElementById('mdn_basics');
 
 function random(min, max){
     const result = Math.floor(Math.random() * (max - min)) + min;
-    console.log('result: ' + result);
     return result;
 }
 
 function randomName(array) {
     let name = array[random(0, array.length)];
-    console.log(name);
+    console.log('random name in array: ' + name);
+    para.textContent(name);
     return name;
 }
-
-para.textContent = randomName(names);
 
 section.innerHTML = ' ';
 
