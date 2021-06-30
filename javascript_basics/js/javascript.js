d6d2a14e489b3253f6c785b935c3a5c7b359a7bf
dahwild22 <danwilderdesign@gmail.com>
2021-06-30 15:19:30 -0600

    BizzBuzz exercise

--- javascript_basics/js/javascript.js
+++ javascript_basics/js/javascript.js
@@ -40,3 +40,9 @@ function lastLetter (string) {
     document.getElementById('fourth_test').innerHTML = 'Original: ' + string + ' ' + 'Result: ' + result;
     return result;
 }
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) {
+    console.log(i);
+  }
