af6e8df21dcf8fa2241589bbfd461c5d864f0500
dahwild22 <danwilderdesign@gmail.com>
2021-07-02 11:06:51 -0600

    started loops tasks

---
+++ loops/index.js
@@ -0,0 +1,9 @@
+let myArray = ['tomatoes', 'chick peas', 'onions', 'rice', 'black beans'];
+    let list = document.createElement('ul');
+
+    // Add your code here
+
+    // Don't edit the code below here!
+
+    const section = document.querySelector('section');
+    section.appendChild(list);
