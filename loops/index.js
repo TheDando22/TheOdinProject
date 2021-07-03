dd54ad6a8767b0949a96ad65cb8dbe035926b856
dahwild22 <danwilderdesign@gmail.com>
2021-07-03 09:35:22 -0600

    switched branch and commit

--- loops/index.js
+++ loops/index.js
@@ -1,7 +1,35 @@
+
+// Active Learning
+const output = document.querySelector('.output');
+output.innerHTML = '';
+
+for (let i = 10;i >= 0;i--){ 
+    const para = document.createElement('p');
+    para.textContent = i;
+    if (i === 10) {
+        para.textContent = 'Countdown' + i;
+    } else if (i === 0) {
+        para.textContent = 'Blast off!';
+    } else {
+        para.textContent = i;
+    }
+    output.appendChild(para);
+}
+
+
+
+// Loops Test your skills 1
 let myArray = ['tomatoes', 'chick peas', 'onions', 'rice', 'black beans'];
     let list = document.createElement('ul');
 
-    // Add your code here
+    for (let i = 0;i < myArray.length;i++) {
+        list = myArray[i];
+        if (i === myArray.length - 1) {
+            list += 'and ' + myArray[i] + '.';
+        } else {
+            list += myArray[i] + ', ';
+        }
+    }
 
     // Don't edit the code below here!
 
